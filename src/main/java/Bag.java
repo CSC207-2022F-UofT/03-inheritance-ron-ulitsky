410f3f6e0598bafc40b4b729abb45ff5a7b1cd9f
Ron Ulitsky <ron.ulitsky@gmail.com>
2022-10-09 23:26:07 -0400

    submission 0

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -5,20 +5,26 @@
  * 1. Introduction to Java helpful.
  */
 
+import java.lang.reflect.Array;
+
 public abstract class Bag {
     /*
-     * TODO: Create the following private instance variables
+     * DONE: Create the following private instance variables
      *       - a String named color
      *       - an int named numberOfContents
      *       - an int named capacity
      *       - an array of Strings named contents
      */
+    private String color;
+    private int numberOfContents = 0;
+    private int capacity;
+    private String[] contents = {};
 
 
 
 
     /*
-     * TODO: Create a constructor that takes two arguments:
+     * DONE: Create a constructor that takes two arguments:
      *       - a String representing the Bag's colour
      *       - an int representing the Bag's capacity
      *
@@ -26,32 +32,50 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
+    public Bag(String color, int capacity){
+        this.color = color;
+        this.capacity = capacity;
+    }
 
 
 
 
     /*
-     * TODO: Create a variety of 'getter' functions.
+     * DONE: Create a variety of 'getter' functions.
      *       These should be named:
      *           - getColor
      *           - getNumberOfContents
      *           - getCapacity
      */
 
+    public String getColor() {
+        return color;
+    }
 
 
+    public int getNumberOfContents() {
+        return numberOfContents;
+    }
+
+
+    public int getCapacity() {
+        return capacity;
+    }
 
     /*
-     * TODO: Create a setter function called setColor which sets the
+     * DONE: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
+    public void setColor(String color) {
+        this.color = color;
+    }
 
 
 
 
 
     /*
-     * TODO: Create a method called addItem that takes in a String
+     * DONE: Create a method called addItem that takes in a String
      *       representing an item in the Bag.
      *       The item is added into the Bag if the number of items
      *       in the bag is < the capacity of the Bag.
@@ -60,13 +84,25 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
+    public void addItem(String item){
+        if(this.numberOfContents < this.capacity){
+            String[] newContents = new String[this.numberOfContents + 1];
+            for(int i = 0; i < this.numberOfContents; i++){
+                newContents[i] = this.contents[i];
+            }
+            newContents[this.contents.length] = item;
+            this.contents = newContents;
+            this.numberOfContents = this.contents.length;
+        }
+
+    }
 
 
 
 
 
     /**
-     * TODO: Create a method called popItem that returns a String.
+     * DONE: Create a method called popItem that returns a String.
      *       The string should be the last item added to this Bag
      *       and the item should be removed from this Bag.
      *       Remember to modify numberOfContents accordingly.
@@ -75,6 +111,20 @@ public abstract class Bag {
      *
      * @return
      */
+    public String popItem(){
+        if(this.numberOfContents > 0){
+            String popped = this.contents[-1];
+            String[] newContents = new String[this.numberOfContents- 1];
+            for(int i = 0; i < this.numberOfContents - 1; i++){
+                newContents[i] = this.contents[i];
+            }
+            this.contents = newContents;
+            this.numberOfContents = this.contents.length;
+            return popped;
+        }else{
+            return null;
+        }
+    }
 
 
 
@@ -86,8 +136,8 @@ public abstract class Bag {
      * @param n the amount to increase this Bag's capacity by
      */
     public void increaseCapacity(int n) {
-        // TODO: Implement this method.
-
+        // DONE: Implement this method.
+        this.capacity += n;
     }
 
     /**
